3c33baf6a487c596b9153b4f458b990259263ac1
Thomas Witherow <twitherow@bellsouth.net>
2021-06-10 14:12:39 -0500

    Post is working

--- src/scripts/PartyForm.js
+++ src/scripts/PartyForm.js
@@ -1,15 +1,47 @@
 
+    import { sendRequest } from "./dataAccess.js"
+
+
+
+    const mainContainer = document.querySelector("#container")
+
+    mainContainer.addEventListener("click", clickEvent => {
+        if (clickEvent.target.id === "submitRequest") {
+            // Get what the user typed into the form fields
+            const parentName = document.querySelector("input[name='parentName']").value
+            const childName = document.querySelector("input[name='childName']").value
+            const numberOfChildren = document.querySelector("input[name='numberOfChildren']").value
+            const partyAddress = document.querySelector("input[name='partyAddress']").value
+            const partyDate = document.querySelector("input[name='partyDate']").value
+            const lengthOfParty = document.querySelector("input[name='lengthOfParty']").value
+    
+         
+            // Make an object out of the user input
+            const dataToSendToAPI = {
+                parentsName: parentName,
+                childsName: childName,
+                numberOfChildren: numberOfChildren,
+                address: partyAddress,
+                date: partyDate,
+                length: lengthOfParty
+            }
+    
+            // Send the data to the API for permanent storage
+            sendRequest(dataToSendToAPI)
+        }
+    })
+    
 
 
 
 export const PartyForm = () => {
     let html = `
         <div class="field">
-            <label class="label" for="parentName">Parents Name</label>
+            <label class="label" for="parentName">Parent's Name</label>
             <input type="text" name="parentName" class="input" />
         </div>
         <div class="field">
-            <label class="label" for="childName">Childs Name</label>
+            <label class="label" for="childName">Child's Name</label>
             <input type="text" name="childName" class="input" />
         </div>
         <div class="field">
@@ -25,11 +57,11 @@ export const PartyForm = () => {
             <input type="date" name="partyDate" class="input" />
         </div>
         <div class="field">
-            <label class="label" for="LengthOfParty">Party Length</label>
-            <input type="number" name="LengthOfParty" class="input" />
+            <label class="label" for="lengthOfParty">Party Length</label>
+            <input type="number" name="lengthOfParty" class="input" />
         </div>
 
-        <button class="button" id="submitRequest">Submit Request</button>
+        <button class="button" id="submitRequest">Get This Party Started!</button>
     `
 
     return html
